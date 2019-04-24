598f2481f1e3a3ed61fb3523c4bcd67a88929790
Sambasivarao velnati <samba.velnati@gmail.com>
2019-04-24 15:25:48 +0530

    Added new web API project

--- GraphQL/AppConfigGQL.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-
-namespace GraphQL
-{
-    public class AppConfigGQL
-    {
-
-    }
-}
---
+++ GraphQLWebAPI.Tests/HelloWorldTest.cs
@@ -0,0 +1,14 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace GraphQLWebAPI.Tests
+{
+    [TestClass]
+    public class HelloWorldTest
+    {
+        [TestMethod]
+        public void HelloWorldTest_Basic()
+        {
+
+        }
+    }
+}
